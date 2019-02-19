37919c9471b33c30f33a2f495fffc8d59a0ed537
Jennifer Cafiero <jcafiero@stevens.edu>
2019-02-19 16:47:58 -0500

    Add travis info, Readme, and unittesting to repo

---
+++ TestGithubRESTAPI.py
@@ -0,0 +1,17 @@
+#Author: Jennifer Cafiero
+#I pledge my honor that I have abided by the Stevens Honor System
+
+import unittest
+from GithubRESTAPI import list_github_user_repos
+
+class TestGithubRestAPI(unittest.TestCase):
+    def testJcafiero(self):
+        self.assertEqual(list_github_user_repos('jcafiero'), [('Courses', 17), ('CS-182', 2), ('flawless', 16), ('FlawlessHacks', 2), ('future_shop', 15), ('GithubRestSSW567',2), ('HackerRank',20), ('HackerRank2DArray', 2), ('HackPrinceton2016', 30), ('HackRU2018', 28), ('hello-world',4), ('hello-world-1',2), ('jcafiero.github.io', 30), ('LargestPalindromeProduct',12),('opencvdrawing',5),('ProjectEuler-59-XOR-Decryption',3),('SSW567ClassifyTriangles',5),('TriangleSSW567',5)])
+
+    #britrollo is a github user with 0 repos
+    def testBritrollo(self):
+        self.assertEqual(list_github_user_repos('britrollo'),[])
+
+if __name__ == '__main__':
+    print('Running unit tests')
+    unittest.main()
