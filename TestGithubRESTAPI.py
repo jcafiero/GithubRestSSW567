491e724e2ea9487bdf6cdc053826e88cf4894dcd
Jennifer Cafiero <jcafiero@stevens.edu>
2019-03-05 14:16:51 -0500

    Increments test case value

--- TestGithubRESTAPI.py
+++ TestGithubRESTAPI.py
@@ -7,9 +7,9 @@ from GithubRESTAPI import list_github_user_repos
 class TestGithubRESTAPI(unittest.TestCase):
     maxDiff = None
     def testJcafiero(self):
-        self.assertEqual(list_github_user_repos('jcafiero'), [('Courses', 17), ('CS-182', 2), ('flawless', 16),  ('future_shop', 15), ('GithubRestSSW567', 9), ('HackerRank',20), ('HackerRank2DArray', 2), ('HackPrinceton2016', 62), ('HackRU2018', 28), ('hello-world', 4), ('hello-world-1', 2), ('jcafiero.github.io', 30), ('LargestPalindromeProduct', 12), ('opencvdrawing', 5), ('ProjectEuler-59-XOR-Decryption', 3),('SSW567ClassifyTriangles', 5), ('TriangleSSW567', 5)])
+        self.assertEqual(list_github_user_repos('jcafiero'), [('Courses', 17), ('flawless', 16),  ('future_shop', 15), ('GithubRestSSW567', 10), ('HackerRank',20), ('HackerRank2DArray', 2), ('HackPrinceton2016', 30), ('HackRU2018', 28), ('hello-world', 4), ('jcafiero.github.io', 30), ('LargestPalindromeProduct', 12), ('ProjectEuler-59-XOR-Decryption', 3),('SSW567ClassifyTriangles', 5), ('TriangleSSW567', 5)])
 
-    #britrollo is a github user with 0 repos 
+    #britrollo is a github user with 0 repos
     def testBritrollo(self):
         self.assertEqual(list_github_user_repos('britrollo'),[])
 
